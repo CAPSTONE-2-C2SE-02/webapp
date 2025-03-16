29f53098c18f514872b911b0a53cd3ec0234ec0d
khoahuyn <khoahuynh2k3hi@gmail.com>
2025-03-16 17:07:27 +0700

    feature:  cloudinary

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import authRouter from "./auth.route.js";
 import userRoutes from './user.route.js';
 import profileRoutes from './profile.route.js';
+import uploadRouter from "./upload.route.js";
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRoutes);
 router.use('/profiles', profileRoutes);
+router.use('/uploads', uploadRouter);
 
 export default router;
---
+++ server/src/routes/upload.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { StatusCodes } from "http-status-codes";
+
+import cloudinary from '../config/cloudinary.config.js';
+import upload from '../middlewares/multer.middlerware.js';
+
+const uploadRouter = express.Router();
+
+uploadRouter.post("/file", upload.single("image"), (req, res) => {
+    cloudinary.uploader.upload(req.file.path, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Error uploading" });
+        }
+        res.status(StatusCodes.CREATED).json({ message: "Image uploaded successfully", data: result });
+    })
+});
+
+export default uploadRouter;
