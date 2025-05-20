4d7684ae941615ffee13ad92c85a64b3c18bff2b
Nguyen Duc <ngocduc8122002@gmail.com>
2025-05-20 23:44:04 +0700

    feat: enhance tour filter functionality and UI components

--- client/src/components/tour/filter/price-filter.tsx
+++ client/src/components/tour/filter/price-filter.tsx
@@ -1,21 +1,23 @@
 import FilterWrapper from "./filter-wrapper";
 import { Slider } from "@/components/ui/slider";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface PriceFilterProps {
   priceRange: number[];
   setPriceRange: (value: number[]) => void;
+  onApply: () => void;
 }
 
-const MAX_PRICE = 1500;
+const MAX_PRICE = 1500000;
 const MIN_PRICE = 0;
 
-const PriceFilter = ({ priceRange, setPriceRange }: PriceFilterProps) => {
+const PriceFilter = ({ priceRange, setPriceRange, onApply }: PriceFilterProps) => {
   return (
     <FilterWrapper title="Price">
-      <div className="flex items-center justify-between gap-4">
+      <div className="flex items-center justify-between gap-2">
         <Input type="text" value={`$ ${priceRange[0]}`} readOnly />
-        <div className="w-5 h-1 rounded-full bg-muted-foreground flex-shrink-0" />
+        <div className="w-3 h-1 rounded-full bg-muted-foreground flex-shrink-0" />
         <Input type="text" value={`$ ${priceRange[1]}`} readOnly />
       </div>
       <Slider
@@ -26,6 +28,7 @@ const PriceFilter = ({ priceRange, setPriceRange }: PriceFilterProps) => {
         value={priceRange}
         onValueChange={setPriceRange}
       />
+      <Button size={"sm"} className="w-full mt-2" onClick={onApply}>Apply</Button>
     </FilterWrapper>
   );
 };
