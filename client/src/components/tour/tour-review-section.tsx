92de45cccbf459860cac501fb6f3f3846b870288
Nguyen Duc <ngocduc8122002@gmail.com>
2025-05-25 16:46:30 +0700

    feat: integrate Mapbox and React Map GL for enhanced map functionality

--- client/src/components/tour/tour-review-section.tsx
+++ client/src/components/tour/tour-review-section.tsx
@@ -1,39 +1,25 @@
 import ReviewCard from "./review-card";
-import { cn } from "@/lib/utils";
 import { useState } from "react";
 import { Button } from "../ui/button";
-import StarRating from "../utils/star-rating";
 import { Review } from "@/lib/types";
 
 interface ReviewsSectionProps {
   reviews: Review[] | undefined;
-  className?: string;
 }
 
-export default function TourReviewsSection({ reviews, className }: ReviewsSectionProps) {
+export default function TourReviewsSection({ reviews }: ReviewsSectionProps) {
   const [displayCount, setDisplayCount] = useState(3);
 
   const displayedReviews = reviews?.slice(0, displayCount);
   const hasMoreReviews = reviews && reviews.length > displayCount;
 
-  const avgRating = reviews && reviews.length > 0 ? reviews?.reduce((acc, review) => acc + review.ratingForTour, 0) / reviews?.length : 0;
 
   const handleLoadMore = () => {
     setDisplayCount(prev => prev + 3);
   };
 
   return (
-    <div className={cn("bg-white p-3 border rounded-lg space-y-4", className)}>
-      <div className="pr-1">
-        <div className="bg-teal-100/20 border border-primary rounded-lg py-3 flex flex-col items-center gap-2 w-full shadow-[4px_4px_oklch(0.392_0.0844_240.76)]">
-          <h3 className="text-base font-semibold text-primary tracking-wide">Overall Rating</h3>
-          <div className="text-3xl font-bold text-primary font-pacifico">{avgRating.toFixed(1)}<span className="text-gray-500 text-lg">/5</span></div>
-          <div className="flex flex-col items-center gap-1">
-            <StarRating size={6} rating={avgRating} />
-            <span className="text-sm text-gray-500 font-medium">based on {reviews?.length} {reviews?.length === 1 ? "review" : "reviews"}</span>
-          </div>
-        </div>
-      </div>
+    <>
       {reviews && reviews.length > 0 ? (
         <div className="space-y-4">
           {displayedReviews?.map((review) => (
@@ -56,6 +42,6 @@ export default function TourReviewsSection({ reviews, className }: ReviewsSectio
           <p>This tour has no reviews yet.</p>
         </div>
       )}
-    </div>
+    </>
   );
 }
--- client/src/components/tour/tour-search-box.tsx
+++ client/src/components/tour/tour-search-box.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Search, X } from "lucide-react";
+import { Map, MapPin, X } from "lucide-react";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import useDebounce from "@/hooks/useDebounce";
@@ -6,8 +6,10 @@ import { useQuery } from "@tanstack/react-query";
 import { searchTours } from "@/services/tours/tour-api";
 import { ScrollArea } from "../ui/scroll-area";
 import TourSearchItem from "./tour-search-item";
+import MapOverview from "../map/map-view/map-overview";
 
 const TourSearchBox = () => {
+  const [isMapOpen, setIsMapOpen] = useState<boolean>(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const debouncedSearch = useDebounce<string>(searchQuery, 1000);
 
@@ -20,45 +22,47 @@ const TourSearchBox = () => {
   const removeSearchQuery = () => setSearchQuery("");
 
   return (
-    <div className="absolute max-w-[800px] w-full left-1/2 bg-white/60 backdrop-blur-sm p-5 rounded-xl border border-border -translate-x-1/2 bottom-0 translate-y-1/3 flex items-center gap-3 z-40">
-      <div className="px-4 py-2.5 rounded-lg bg-white flex items-center justify-center gap-4 border border-border flex-1 relative">
-        <MapPin className="size-4 text-primary" />
-        <input
-          type="text"
-          className="border-none outline-none flex-1 text-sm placeholder:text-sm"
-          placeholder="Where are you going?"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        {searchQuery && (
-          <Button variant={"ghost"} size={"icon"} className="absolute right-0.5" onClick={removeSearchQuery}>
-            <X className="text-red-500 size-3" />
-          </Button>
-        )}
-      </div>
-      <Button className="text-white h-[40px]">
-        <Search className="size-4" />
-        Search
-      </Button>
-
-      {/* Search result */}
-      {searchQuery && (
-        <div className="absolute top-16 max-w-[646px] w-full bg-white p-2 rounded-lg flex flex-col border border-muted shadow-md">
-          <p className="text-xs text-center font-medium text-primary">Search result for "{searchQuery}"</p>
-          {isLoading && <p className="">Searching tour...</p>}
-          {data?.result && data.result.length <= 0 && <p className="p-3 mt-2 text-center text-sm rounded-md bg-gray-100">Not found tours</p>}
-          {data?.result && data.result.length > 0 && (
-            <ScrollArea className="max-h-80 w-full mt-2">
-              <div className="space-y-1 w-full">
-                {data?.success && data.result?.map((tour) => (
-                  <TourSearchItem key={tour._id} tour={tour} />
-                ))}
-              </div>
-            </ScrollArea>
+    <>
+      <div className="absolute max-w-[800px] w-full left-1/2 bg-white/60 backdrop-blur-sm p-5 rounded-xl border border-border -translate-x-1/2 bottom-0 translate-y-1/3 flex items-center gap-3 z-40">
+        <div className="px-4 py-2.5 rounded-lg bg-white flex items-center justify-center gap-4 border border-border flex-1 relative">
+          <MapPin className="size-4 text-primary" />
+          <input
+            type="text"
+            className="border-none outline-none flex-1 text-sm placeholder:text-sm"
+            placeholder="Where are you going?"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          {searchQuery && (
+            <Button variant={"ghost"} size={"icon"} className="absolute right-0.5" onClick={removeSearchQuery}>
+              <X className="text-red-500 size-3" />
+            </Button>
           )}
         </div>
-      )}
-    </div>
+        <Button className="text-white h-[40px]" onClick={() => setIsMapOpen(true)}>
+          <Map className="size-4" /> Map
+        </Button>
+
+        {/* Search result */}
+        {searchQuery && (
+          <div className="absolute top-16 max-w-[646px] w-full bg-white p-2 rounded-lg flex flex-col border border-muted shadow-md">
+            <p className="text-xs text-center font-medium text-primary">Search result for "{searchQuery}"</p>
+            {isLoading && <p className="">Searching tour...</p>}
+            {data?.result && data.result.length <= 0 && <p className="p-3 mt-2 text-center text-sm rounded-md bg-gray-100">Not found tours</p>}
+            {data?.result && data.result.length > 0 && (
+              <ScrollArea className="max-h-80 w-full mt-2">
+                <div className="space-y-1 w-full">
+                  {data?.success && data.result?.map((tour) => (
+                    <TourSearchItem key={tour._id} tour={tour} />
+                  ))}
+                </div>
+              </ScrollArea>
+            )}
+          </div>
+        )}
+      </div>
+      <MapOverview isOpen={isMapOpen} onChange={setIsMapOpen} />
+    </>
   );
 };
 
